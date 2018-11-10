4129ea0130ff2d50428a829494ebdf2029071cc6
Vladimir Grichina <vgrichina@componentix.com>
2018-11-09 20:15:19 -0800

    More changes towards working Token contract
    Verified back-forth communication with state storage outside of
    WebAssembly.

--- templates/token_contract_ts/assembly/main.ts
+++ templates/token_contract_ts/assembly/main.ts
@@ -1,43 +1,88 @@
 import "allocator/arena";
 import { u128 } from "./bignum/integer/u128";
 
-interface Storage {
-  setItem(key:String, value:String): void;
-  getItem(key:String): String;
-  removeItem(key:String): void;
-}
+export { memory };
 
 type Address = u128;
 type MoneyNumber = u128;
 
+class ContractContext {
+  sender: Address;
+}
+
+declare function _near_globalStorage_setItem(key: string, value: string): void;
+declare function _near_globalStorage_getItem(key: string): string;
+declare function _near_globalStorage_removeItem(key: string): void;
+
+class GlobalStorage {
+  setItem(key: string, value: string): void {
+    _near_globalStorage_setItem(key, value);
+  }
+  getItem(key: string): string {
+    return _near_globalStorage_getItem(key);
+  }
+  removeItem(key: string): void {
+    _near_globalStorage_removeItem(key);
+  }
+  setU128(key: string, value: u128): void {
+    this.setItem(key, value.toString());
+  }
+  getU128(key: string): u128 {
+    return u128.fromString(this.getItem(key) || "0");
+  }
+}
+
+var globalStorage: GlobalStorage;
+var contractContext: ContractContext;
+
+function balanceKey(address: Address): string {
+  return "balances:" + address.toString(16);
+}
+
+function approvedKey(from: Address, to: Address): string {
+  return "approved:" + from.toString(16) + ":" +  to.toString(16);
+}
+
 export function totalSupply(): MoneyNumber {
-  // TODO
+  // TODO: There should be some way to issue tokens
   return u128.fromI32(1337);
 }
 
 export function balanceOf(tokenOwner: Address): MoneyNumber {
-  // TODO
-  return u128.Zero;
+  let ownerKey = balanceKey(tokenOwner);
+  return u128.fromString(globalStorage.getItem(ownerKey) || "0");
 }
 
 export function allowance(tokenOwner: Address, spender: Address): MoneyNumber {
-  // TODO
-  return u128.Zero;
+  let spenderKey = approvedKey(tokenOwner, spender);
+  return u128.fromString(globalStorage.getItem(spenderKey));
 }
 
 export function transfer(to: Address, tokens: MoneyNumber): boolean {
-  // TODO
-  return false;
+  let fromKey = balanceKey(contractContext.sender);
+  let toKey = balanceKey(to);
+  globalStorage.setU128(fromKey, globalStorage.getU128(fromKey) - tokens);
+  globalStorage.setU128(toKey, globalStorage.getU128(toKey) + tokens);
+  onTransfer(contractContext.sender, to, tokens);
+  return true;
 }
 
 export function approve(spender: Address, tokens: MoneyNumber): boolean {
-  // TODO
-  return false;
+  let spenderKey = approvedKey(contractContext.sender, spender);
+  globalStorage.setU128(spenderKey, tokens);
+  onApproval(contractContext.sender, spender, tokens);
+  return true;
 }
 
 export function transferFrom(from: Address, to: Address, tokens: MoneyNumber): boolean {
-  // TODO
-  return false;
+  let fromKey = balanceKey(from);
+  let toKey = balanceKey(to);
+  let spenderKey = approvedKey(contractContext.sender, to);
+  globalStorage.setU128(fromKey, globalStorage.getU128(fromKey) - tokens);
+  globalStorage.setU128(spenderKey, globalStorage.getU128(spenderKey) - tokens);
+  globalStorage.setU128(toKey, globalStorage.getU128(toKey) - tokens);
+  onTransfer(from, to, tokens);
+  return true;
 }
 
 declare function onTransfer(from: Address, to: Address, tokens: MoneyNumber): void;
--- templates/token_contract_ts/src/main.js
+++ templates/token_contract_ts/src/main.js
@@ -1,7 +1,28 @@
+let contractModule;
 loader.instantiateStreaming(fetch("../out/main.wasm"), {
   main: {
-    sayHello() {
-      console.log("Hello from WebAssembly!");
+    _near_globalStorage_getItem(key) {
+      key = contractModule.getString(key);
+      value = localStorage.getItem(key)
+      console.log("getItem", key, value);
+      return value ? contractModule.newString(value) : null;
+    },
+    _near_globalStorage_setItem(key, value) {
+      key = contractModule.getString(key);
+      value = contractModule.getString(value);
+      console.log("setItem", key, value);
+      localStorage.setItem(key, value);
+    },
+    _near_globalStorage_removeItem(key) {
+      key = contractModule.getString(key);
+      console.log("removeItem", key);
+      localStorage.removeItem(key);
+    },
+    onTransfer(from, to, tokens) {
+      console.log("onTransfer", from, to, tokens);
+    },
+    onApproval(tokenOwner, spender, tokens) {
+      console.log("onTransfer", tokenOwner, spender, tokens);
     }
   },
   env: {
@@ -9,7 +30,9 @@ loader.instantiateStreaming(fetch("../out/main.wasm"), {
       console.error("abort called at main.ts:" + line + ":" + column);
     }
   },
-}).then(contractModule => {
+}).then(module => {
+  contractModule = module;
+  window.contractModule = module;
   contractModule.readBytes = function(ptr, length) {
     var result = [];
     for (let i = 0; i < length; i++) {
@@ -18,11 +41,31 @@ loader.instantiateStreaming(fetch("../out/main.wasm"), {
     return result;
   }
 
-  contractModule.readUInt128 = function(ptr) {
+  // TODO: Make sure signs are handled properly
+  contractModule.readU128 = function(ptr) {
     return bigInt.fromArray(this.readBytes(ptr, 16).reverse(), 0x100);
   }
 
-  const numPtr = contractModule.totalSupply();
-  console.log("mem", contractModule.readBytes(numPtr, 16));
-  document.getElementById("container").textContent = "Result: " + contractModule.readUInt128(contractModule.totalSupply()).toString();
+  contractModule.newU128 = function(big) {
+    // TODO: Figure out clean way to do memory management. If our contracts are short-lived enough might be viable always allocating new memory blocks.
+    const bytesArray = big.toArray(0x100).value.reverse();
+    console.assert(bytesArray.length <= 16, "Passed BigInteger should fit into 128-bit");
+    while (bytesArray.length < 16) {
+      bytesArray.push(0);
+    }
+    const ptr = this.memory.allocate(bytesArray.length);
+    for (let i = 0; i < 16; i++) {
+      this.I8[ptr + i] = bytesArray[i];
+    }
+    return ptr;
+  }
+
+  document.getElementById("container").innerHTML =
+    "Total Supply: " + contractModule.readU128(contractModule.totalSupply());
+
+  console.log("U128 roundtrip", contractModule.readU128(contractModule.newU128(bigInt("12345"))));
+
+  const account2 = contractModule.newU128(bigInt("22222222222222222222222222222222", 16));
+  contractModule.transfer(account2, contractModule.newU128(bigInt("12345")));
+  console.log("balance 2", contractModule.readU128(contractModule.balanceOf(account2)));
 }).catch(console.error);
