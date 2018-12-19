9efa5fbc5c320a1e9861fe50a7d88380600cc686
Vladimir Grichina <vgrichina@gmail.com>
2018-12-19 15:56:12 -0800

    Test `approve` and `transferFrom` methods

--- templates/token_contract_ts/src/main.js
+++ templates/token_contract_ts/src/main.js
@@ -5,21 +5,32 @@ const contractName = "studio-M5X4N4R";
 async function runTest(tokenContract) {
   let alice = "3x9az88Dkbxa6tkKByxqEn7jBTJCJCD4dVvou49L24ET";
   let bob = "9jLkNAaW9E47LQMHvjohy2uAAyr1331bAxgJKFRU7wF6";
+  let eve = "9xkyoPm8xs4PFuonyoTMMbHxi4crcvmpsBsFkukYrELJ";
 
-  console.log("balanceOf alice", await tokenContract.balanceOf({tokenOwner: alice}));
-  console.log("balanceOf bob", await tokenContract.balanceOf({tokenOwner: bob}));
-
-  await sleep(1000);
   await tokenContract._init({ initialOwner: bob });
   await sleep(1000);
   console.log("balanceOf alice", await tokenContract.balanceOf({tokenOwner: alice}));
   console.log("balanceOf bob", await tokenContract.balanceOf({tokenOwner: bob}));
 
-  await sleep(1000);
   await tokenContract.transfer({ from: bob, to: alice, tokens: "100" });
   await sleep(2000);
   console.log("balanceOf alice", await tokenContract.balanceOf({tokenOwner: alice}));
   console.log("balanceOf bob", await tokenContract.balanceOf({tokenOwner: bob}));
+  console.log("balanceOf eve", await tokenContract.balanceOf({tokenOwner: eve}));
+
+  await tokenContract.approve({ spender: eve, tokens: "100" });
+  await sleep(2000);
+  console.log("balanceOf alice", await tokenContract.balanceOf({tokenOwner: alice}));
+  console.log("balanceOf bob", await tokenContract.balanceOf({tokenOwner: bob}));
+  console.log("balanceOf eve", await tokenContract.balanceOf({tokenOwner: eve}));
+
+  // TODO: Use "eve" as sender
+  // TODO: Looks like atomicity of contract method not guaranteed?
+  await tokenContract.transferFrom({ from: bob, to: eve, tokens: "50" });
+  await sleep(2000);
+  console.log("balanceOf alice", await tokenContract.balanceOf({tokenOwner: alice}));
+  console.log("balanceOf bob", await tokenContract.balanceOf({tokenOwner: bob}));
+  console.log("balanceOf eve", await tokenContract.balanceOf({tokenOwner: eve}));
 }
 
 // TODO: What's proper way to wait for method execution
@@ -31,7 +42,7 @@ function sleep(time) {
 
 // TODO: Introspection of contract methods
 let tokenContract = {};
-["totalSupply", "balanceOf"].forEach((methodName) => {
+["totalSupply", "balanceOf", "allowance"].forEach((methodName) => {
   tokenContract[methodName] = async function(args) {
     //console.log(methodName, args);
     args = args || {};
@@ -39,7 +50,7 @@ let tokenContract = {};
     return response.result;
   };
 });
-["_init", "transfer"].forEach((methodName) => {
+["_init", "transfer", "approve", "transferFrom"].forEach((methodName) => {
   tokenContract[methodName] = async function(args) {
     console.log(methodName, args);
     args = args || {};
