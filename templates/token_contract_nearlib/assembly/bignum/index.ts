1c3dbc974a9548b22facc1f86628947eb8233d79
Vladimir Grichina <vgrichina@gmail.com>
2019-01-10 23:36:56 -0800

    Remove bignum from nearlib template as well

--- templates/token_contract_nearlib/assembly/bignum/index.ts
+++
@@ -1,8 +0,0 @@
-
-import { u128 } from "./integer/u128";
-import { u256 } from "./integer/u256";
-
-export {
-  u128,
-  u256,
-};
--- templates/token_contract_nearlib/assembly/main.ts
+++ templates/token_contract_nearlib/assembly/main.ts
@@ -3,10 +3,10 @@ export { memory };
 
 import { contractContext, globalStorage, near } from "./near";
 
-import { u128 } from "./bignum/integer/safe/u128";
-
 // --- contract code goes below
 // --- bigints temporarily stringly typed, need support in bindgen
+
+// TODO: Now that it's all u64 needs either safe math wrapper or asserts all over the place
 
 function balanceKey(address: string): string {
   return "balances:" + address;
@@ -16,10 +16,10 @@ function approvedKey(from: string, to: string): string {
   return "approved:" + from + ":" +  to;
 }
 
-let TOTAL_SUPPLY = u128.fromI32(1000000);
+let TOTAL_SUPPLY: u64 = 1000000;
 export function _init(initialOwner: string): void {
   near.log("initialOwner: " + initialOwner);
-  globalStorage.setU128(balanceKey(initialOwner), TOTAL_SUPPLY);
+  globalStorage.setU64(balanceKey(initialOwner), TOTAL_SUPPLY);
 }
 
 export function totalSupply(): string {
@@ -43,17 +43,17 @@ export function transfer(to: string, tokens: string): boolean {
   near.log("transfer: " + to + " tokens: " + tokens);
   let fromKey = balanceKey(contractContext.sender.toString());
   let toKey = balanceKey(to);
-  let tokensNum = u128.fromString(tokens);
+  let tokensNum = U64.parseInt(tokens);
   near.log("from: " + fromKey + " to: " + toKey);
-  globalStorage.setU128(fromKey, globalStorage.getU128(fromKey) - tokensNum);
-  globalStorage.setU128(toKey, globalStorage.getU128(toKey) + tokensNum);
+  globalStorage.setU64(fromKey, globalStorage.getU64(fromKey) - tokensNum);
+  globalStorage.setU64(toKey, globalStorage.getU64(toKey) + tokensNum);
   //onTransfer(contractContext.sender, to, tokens);
   return true;
 }
 
 export function approve(spender: string, tokens: string): boolean {
   let spenderKey = approvedKey(contractContext.sender.toString(), spender);
-  globalStorage.setU128(spenderKey, u128.fromString(tokens));
+  globalStorage.setU64(spenderKey, U64.parseInt(tokens));
   //onApproval(contractContext.sender, spender, tokens);
   return true;
 }
@@ -62,10 +62,10 @@ export function transferFrom(from: string, to: string, tokens: string): boolean 
   let fromKey = balanceKey(from);
   let toKey = balanceKey(to);
   let spenderKey = approvedKey(contractContext.sender.toString(), to);
-  let tokensNum = u128.fromString(tokens);
-  globalStorage.setU128(fromKey, globalStorage.getU128(fromKey) - tokensNum);
-  globalStorage.setU128(spenderKey, globalStorage.getU128(spenderKey) - tokensNum);
-  globalStorage.setU128(toKey, globalStorage.getU128(toKey) + tokensNum);
+  let tokensNum = U64.parseInt(tokens);
+  globalStorage.setU64(fromKey, globalStorage.getU64(fromKey) - tokensNum);
+  globalStorage.setU64(spenderKey, globalStorage.getU64(spenderKey) - tokensNum);
+  globalStorage.setU64(toKey, globalStorage.getU64(toKey) + tokensNum);
   //onTransfer(from, to, tokens);
   return true;
 }
