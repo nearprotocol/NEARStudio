3745cdc314c77547a83a0fd2ef5ba9d50ca22e29
Illia Polosukhin <ilblackdragon@gmail.com>
2019-01-29 16:44:09 -0800

    Add JS to load contract and nearlib into window

--- templates/token_contract_ts/src/main.js
+++ templates/token_contract_ts/src/main.js
@@ -1,1 +1,33 @@
-// Placeholder for UI javascript
+// Loads nearlib and this contract into window scope.
+
+let initPromise;
+window.initContract = function () {
+  if (window.contract) {
+    return Promise.resolve();
+  }
+  if (!initPromise) {
+    initPromise = doInitContract();
+  }
+  return initPromise;
+}
+
+async function doInitContract() {
+  const config = await nearlib.dev.getConfig();
+  console.log("nearConfig", config);
+  
+  window.near = await nearlib.dev.connect();
+  
+  window.contract = await near.loadContract(config.contractName, {
+    viewMethods: ["totalSupply", "balanceOf", "allowance"],
+    changeMethods: ["_init", "transfer", "approve", "transferFrom"],
+    sender: nearlib.dev.myAccountId
+  });
+}
+
+function sleep(time) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(resolve, time);
+  });
+}
+
+initContract().catch(console.error);
