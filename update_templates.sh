5f8c9457f63c147c348d23d74ffb103751e3bdc7
Vladimir Grichina <vgrichina@gmail.com>
2019-03-04 20:07:31 -0800

    More robust copying of json encoder/decoder

--- update_templates.sh
+++ update_templates.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 # See https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
-echo templates/*/assembly | xargs -n 1 cp node_modules/near-runtime-ts/near.ts
-echo templates/*/assembly/json | xargs -n 1 cp -R node_modules/assemblyscript-json/assembly/*
+echo templates/*/assembly | xargs -n 1 -t cp node_modules/near-runtime-ts/near.ts
+JSON_DIRS="$(echo templates/*/assembly | xargs -n 1 printf "%s/json\n")"
+echo "$JSON_DIRS" | xargs -n 1 -t mkdir -p
+echo "$JSON_DIRS" | xargs -n 1 -t cp -R node_modules/assemblyscript-json/assembly/{encoder,decoder}.ts
