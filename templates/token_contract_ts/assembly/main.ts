ba9b7a8e3f167f9992b15b1151f1924447fdf1c2
Vladimir Grichina <vgrichina@gmail.com>
2019-03-05 00:38:14 -0800

    Use u64 in token contract

--- templates/token_contract_ts/assembly/main.ts
+++ templates/token_contract_ts/assembly/main.ts
@@ -25,49 +25,47 @@ export function totalSupply(): string {
   return TOTAL_SUPPLY.toString();
 }
 
-export function balanceOf(tokenOwner: string): string {
+export function balanceOf(tokenOwner: string): u64 {
   let ownerKey = balanceKey(tokenOwner);
   near.log("balanceOf: " + tokenOwner);
-  let result = globalStorage.getItem(ownerKey) || "0";
-  near.log("result: " + result);
+  let result = globalStorage.getU64(ownerKey);
+  near.log("result: " + result.toString());
   return result;
 }
 
-export function allowance(tokenOwner: string, spender: string): string {
+export function allowance(tokenOwner: string, spender: string): u64 {
   let spenderKey = approvedKey(tokenOwner, spender);
-  return globalStorage.getItem(spenderKey);
+  return globalStorage.getU64(spenderKey);
 }
 
-export function transfer(to: string, tokens: string): boolean {
-  near.log("transfer: " + to + " tokens: " + tokens);
+export function transfer(to: string, tokens: u64): boolean {
+  near.log("transfer: " + to + " tokens: " + tokens.toString());
   let fromKey = balanceKey(contractContext.sender);
   let toKey = balanceKey(to);
-  let tokensNum = U64.parseInt(tokens);
   near.log("from: " + fromKey + " to: " + toKey);
   let fromAmount = globalStorage.getU64(fromKey);
-  assert(fromAmount >= tokensNum, "not enough tokens on account");
-  globalStorage.setU64(fromKey, fromAmount - tokensNum);
-  globalStorage.setU64(toKey, globalStorage.getU64(toKey) + tokensNum);
+  assert(fromAmount >= tokens, "not enough tokens on account");
+  globalStorage.setU64(fromKey, fromAmount - tokens);
+  globalStorage.setU64(toKey, globalStorage.getU64(toKey) + tokens);
   return true;
 }
 
-export function approve(spender: string, tokens: string): boolean {
+export function approve(spender: string, tokens: u64): boolean {
   let spenderKey = approvedKey(contractContext.sender, spender);
-  globalStorage.setU64(spenderKey, U64.parseInt(tokens));
+  globalStorage.setU64(spenderKey, tokens);
   return true;
 }
 
-export function transferFrom(from: string, to: string, tokens: string): boolean {
+export function transferFrom(from: string, to: string, tokens: u64): boolean {
   let fromKey = balanceKey(from);
   let toKey = balanceKey(to);
   let spenderKey = approvedKey(contractContext.sender, to);
-  let tokensNum = U64.parseInt(tokens);
   let fromAmount = globalStorage.getU64(fromKey);
-  assert(fromAmount >= tokensNum, "not enough tokens on account");
-  globalStorage.setU64(fromKey, fromAmount - tokensNum);
+  assert(fromAmount >= tokens, "not enough tokens on account");
+  globalStorage.setU64(fromKey, fromAmount - tokens);
   let approvedAmount = globalStorage.getU64(spenderKey)
-  assert(fromAmount >= tokensNum, "not enough tokens approved");
-  globalStorage.setU64(spenderKey, approvedAmount - tokensNum);
-  globalStorage.setU64(toKey, globalStorage.getU64(toKey) + tokensNum);
+  assert(fromAmount >= tokens, "not enough tokens approved");
+  globalStorage.setU64(spenderKey, approvedAmount - tokens);
+  globalStorage.setU64(toKey, globalStorage.getU64(toKey) + tokens);
   return true;
 }
