1bd7845951a053a616666897b79fcc8e753b8947
Peter DePaulo <peterdepaulo@gmail.com>
2019-03-13 21:35:37 -0700

    remove whitespace and turn off autobuild of docs

--- templates/00-hello-world/assembly/main.ts
+++ templates/00-hello-world/assembly/main.ts
@@ -6,14 +6,10 @@ import { contractContext, globalStorage, near } from "./near";
 // --- contract code goes below
 
 // >> hello-snippet
-
 // To be able to call this function in the contract we need to export it
 // using `export` keyword. 
 
 export function hello(): string {
   return "Hello, World!";
 }
-
 // << hello-snippet
-
-
