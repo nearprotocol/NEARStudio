6ed75034fdc9d5783257ed7873fe6115eec63d36
Illia Polosukhin <ilblackdragon@gmail.com>
2019-04-23 10:44:44 -0700

    Changed token contract template to use collections

--- templates/05-token-contract/assembly/main.ts
+++ templates/05-token-contract/assembly/main.ts
@@ -1,23 +1,18 @@
 import "allocator/arena";
 export { memory };
 
-import { context, storage, near } from "./near";
+import { context, storage, near, collections } from "./near";
 
 // --- contract code goes below
 
-function balanceKey(address: string): string {
-  return "balances:" + address;
-}
-
-function approvedKey(from: string, to: string): string {
-  return "approved:" + from + ":" +  to;
-}
+const balances = collections.map<string, u64>("b:");
+const approves = collections.map<string, u64>("a:");
 
 let TOTAL_SUPPLY: u64 = 1000000;
 export function init(initialOwner: string): void {
   near.log("initialOwner: " + initialOwner);
   assert(storage.getItem("init") == null, "Already initialized token supply");
-  storage.setU64(balanceKey(initialOwner), TOTAL_SUPPLY);
+  balances.set(initialOwner, TOTAL_SUPPLY);
   storage.setItem("init", "done");
 }
 
@@ -26,46 +21,36 @@ export function totalSupply(): string {
 }
 
 export function balanceOf(tokenOwner: string): u64 {
-  let ownerKey = balanceKey(tokenOwner);
   near.log("balanceOf: " + tokenOwner);
-  let result = storage.getU64(ownerKey);
+  let result = balances.get(tokenOwner);
   near.log("result: " + result.toString());
   return result;
 }
 
 export function allowance(tokenOwner: string, spender: string): u64 {
-  let spenderKey = approvedKey(tokenOwner, spender);
-  return storage.getU64(spenderKey);
+  return approves.get(tokenOwner + ":" + spender);
 }
 
 export function transfer(to: string, tokens: u64): boolean {
-  near.log("transfer: " + to + " tokens: " + tokens.toString());
-  let fromKey = balanceKey(context.sender);
-  let toKey = balanceKey(to);
-  near.log("from: " + fromKey + " to: " + toKey);
-  let fromAmount = storage.getU64(fromKey);
+  near.log("transfer from" + context.sender + " to " + to + " tokens: " + tokens.toString());
+  let fromAmount = balances.get(context.sender);
   assert(fromAmount >= tokens, "not enough tokens on account");
-  storage.setU64(fromKey, fromAmount - tokens);
-  storage.setU64(toKey, storage.getU64(toKey) + tokens);
+  balances.set(context.sender, fromAmount - tokens);
+  balances.set(to, balances.get(to) + tokens);
   return true;
 }
 
 export function approve(spender: string, tokens: u64): boolean {
-  let spenderKey = approvedKey(context.sender, spender);
-  storage.setU64(spenderKey, tokens);
+  approves.set(context.sender + ":" + spender, tokens);
   return true;
 }
 
 export function transferFrom(from: string, to: string, tokens: u64): boolean {
-  let fromKey = balanceKey(from);
-  let toKey = balanceKey(to);
-  let spenderKey = approvedKey(context.sender, to);
-  let fromAmount = storage.getU64(fromKey);
+  let fromAmount = balances.get(from);
   assert(fromAmount >= tokens, "not enough tokens on account");
-  storage.setU64(fromKey, fromAmount - tokens);
-  let approvedAmount = storage.getU64(spenderKey)
-  assert(fromAmount >= tokens, "not enough tokens approved");
-  storage.setU64(spenderKey, approvedAmount - tokens);
-  storage.setU64(toKey, storage.getU64(toKey) + tokens);
+  let approvedAmount = approves.get(from + ":" + to);
+  assert(tokens <= approvedAmount, "not enough tokens approved to transfer");
+  balances.set(from, fromAmount - tokens);
+  balances.set(to, balances.get(to) + tokens);
   return true;
 }
