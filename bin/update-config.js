988f72b6cf412cb9915d1230d3e474c93a7941b7
Vladimir Grichina <vgrichina@gmail.com>
2019-07-02 18:53:03 -0700

    Added script used to update config based on env vars

---
+++ bin/update-config.js
@@ -0,0 +1,6 @@
+const fs = require('fs');
+const config = JSON.parse(fs.readFileSync('config.json'));
+config.nodeUrl = process.env.REACT_APP_NODE_URL || config.nodeUrl;
+config.contractHelper = process.env.REACT_APP_ACCOUNT_HELPER_URL || config.contractHelper;
+config.pages = process.env.FIDDLE_PAGES_URL || config.pages;
+fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
