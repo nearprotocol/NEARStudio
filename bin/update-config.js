d0d6dd7281ae2ce1bc2b0dbded010b1fc9981a5c
Vladimir Grichina <vgrichina@gmail.com>
2019-07-03 18:37:18 -0700

    Update fiddle API URL based on env var

--- bin/update-config.js
+++ bin/update-config.js
@@ -2,5 +2,6 @@ const fs = require('fs');
 const config = JSON.parse(fs.readFileSync('config.json'));
 config.nodeUrl = process.env.REACT_APP_NODE_URL || config.nodeUrl;
 config.contractHelper = process.env.REACT_APP_ACCOUNT_HELPER_URL || config.contractHelper;
+config.fiddle = process.env.FIDDLE_API_URL || config.fiddle;
 config.pages = process.env.FIDDLE_PAGES_URL || config.pages;
 fs.writeFileSync('config.json', JSON.stringify(config, null, 2));
