c5a8b1a67f91e7cea2f6cb679191d60698f33bca
Vladimir Grichina <vgrichina@gmail.com>
2019-07-16 00:33:36 -0700

    Add Sign In using wallet to token contract

--- templates/05-token-contract/src/main.js
+++ templates/05-token-contract/src/main.js
@@ -1,15 +1,38 @@
-// Loads nearlib and this contract into window scope.
+// Connects to NEAR and provides `near`, `walletAccount` and `contract` objects in `window` scope
+async function connect() {
+  // Initializing connection to the NEAR node.
+  window.near = await nearlib.connect(Object.assign(nearConfig, { deps: { keyStore: new nearlib.keyStores.BrowserLocalStorageKeyStore() }}));
 
-async function initContract() {
-  console.log("nearConfig", nearConfig);
-  
-  window.near = await nearlib.dev.connect();
-  
+  // Needed to access wallet login
+  window.walletAccount = new nearlib.WalletAccount(window.near);
+
+  // Initializing our contract APIs by contract name and configuration.
   window.contract = await near.loadContract(nearConfig.contractName, {
     viewMethods: ["totalSupply", "balanceOf", "allowance"],
     changeMethods: ["init", "transfer", "approve", "transferFrom"],
-    sender: nearlib.dev.myAccountId
+    sender: window.walletAccount.getAccountId()
   });
 }
 
-window.nearInitPromise = initContract().catch(console.error);
+function updateUI() {
+  if (!window.walletAccount.getAccountId()) {
+    Array.from(document.querySelectorAll('.sign-in')).map(it => it.style = "display: block;");
+  } else {
+    Array.from(document.querySelectorAll('.after-sign-in')).map(it => it.style = "display: block;");
+  }
+}
+
+// Log in user using NEAR Wallet on "Sign In" button click
+document.querySelector('.sign-in .btn').addEventListener('click', () => {
+  walletAccount.requestSignIn(nearConfig.contractName, 'NEAR Studio Counter');
+});
+
+document.querySelector('.sign-out .btn').addEventListener('click', () => {
+  walletAccount.signOut();
+  // TODO: Move redirect to .signOut() ^^^
+  window.location.replace(window.location.origin + window.location.pathname);
+});
+
+window.nearInitPromise = connect()
+  .then(updateUI)
+  .catch(console.error);
