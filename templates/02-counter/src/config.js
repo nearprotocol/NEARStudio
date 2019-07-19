74dfeef3ac8a7f63de6e32f1bf1fb613f8744833
Vladimir Grichina <vgrichina@gmail.com>
2019-07-19 00:56:45 -0700

    Update templates based on latest near-shell/blank_project

--- templates/02-counter/src/config.js
+++ templates/02-counter/src/config.js
@@ -7,15 +7,36 @@
             case 'production':
             case 'development':
                 return {
-                    nodeUrl: 'https://studio.nearprotocol.com/devnet',
-                    helperUrl: 'https://studio.nearprotocol.com/contract-api',
+                    networkId: 'default',
+                    nodeUrl: 'https://rpc.nearprotocol.com',
                     contractName: CONTRACT_NAME,
+                    walletUrl: 'https://wallet.nearprotocol.com',
+                    initialBalance: 100000000,
                 };
             case 'local':
+                return {
+                    networkId: 'local',
+                    nodeUrl: 'http://localhost:3030',
+                    keyPath: '~/.near/validator_key.json',
+                    contractName: CONTRACT_NAME,
+                    initialBalance: 100000000,
+                };
             case 'test':
                 return {
+                    networkId: 'local',
                     nodeUrl: 'http://localhost:3030',
-                    contractName: CONTRACT_NAME
+                    contractName: CONTRACT_NAME,
+                    masterAccount: 'test.near',
+                    initialBalance: 100000000,
+                };
+            case 'test-remote':
+            case 'ci':
+                return {
+                    networkId: 'shared-test',
+                    nodeUrl: 'http://shared-test.nearprotocol.com:3030',
+                    contractName: CONTRACT_NAME,
+                    masterAccount: 'test.near',
+                    initialBalance: 100000000,
                 };
             default:
                 throw Error(`Unconfigured environment '${env}'. Can be configured in src/config.js.`);
