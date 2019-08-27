9359744dcffd6a3f39dc912fa68dc8685772bde5
Willem Wyndham <willem.neal@gmail.com>
2019-08-26 21:52:53 -0400

    Update templates

--- update_templates.sh
+++ update_templates.sh
@@ -5,10 +5,23 @@ set -euxo pipefail
 TEMPLATE_DIRS="$(ls -d templates/0*)"
 
 for template in $TEMPLATE_DIRS; do
-  (cd $template; yarn install && rm -rf node_modules/.bin)
+  echo $template
+  cp ./templates/setup.js $template/setup.js
+  (cd $template && (rm yarn.lock || true) && yarn install --prod)
+  rm -rf $template/node_modules.bk;
+  mkdir $template/node_modules.bk
   for package in $(ls -d $template/node_modules/*); do
-    if [ ! -d $package/assembly ]; then
-      rm -rf $package;
+    if [ -d $package/assembly ]; then
+      parent=$(basename $package)
+      echo $parent
+      mkdir $template/node_modules.bk/$parent;
+      cp -r ./$template/node_modules/$parent/assembly $template/node_modules.bk/$parent/
+      # TODO use list files to only copy files needed to compile
+      # for file in $(./node_modules/.bin/asc --listFiles assembly/index.ts --baseDir ./$package 2>&1 > /dev/null | grep -v \~lib); do
+      #   cp -p ./$template/node_modules/$parent/$file ./$template/node_modules.bk/$parent/$file
+      # done
     fi
   done
+  rm -rf $template/node_modules;
+  mv $template/node_modules.bk $template/node_modules;
 done
