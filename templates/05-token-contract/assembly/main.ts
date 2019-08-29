3cfce1e8ffb88bfa61fd4097012b5f55246b2aaf
Jane <jane.degtiareva@gmail.com>
2019-08-29 11:56:56 -0700

    Get counter contract to compile

--- templates/05-token-contract/assembly/main.ts
+++ templates/05-token-contract/assembly/main.ts
@@ -1,17 +1,17 @@
 //@nearfile
-import { context, storage, near, collections } from "near-runtime-ts";
+import { context, storage, logging, collections, PersistentMap } from "near-runtime-ts";
 
 // --- contract code goes below
 
-let balances = collections.map<string, u64>("b:");
-let approves = collections.map<string, u64>("a:");
+let balances = new PersistentMap<string, u64>("b:");
+let approves = new PersistentMap<string, u64>("a:");
 
 let TOTAL_SUPPLY: u64 = 1000000;
 export function init(initialOwner: string): void {
-  near.log("initialOwner: " + initialOwner);
-  assert(storage.getItem("init") == null, "Already initialized token supply");
+  logging.log("initialOwner: " + initialOwner);
+  assert(storage.get<string>("init") == null, "Already initialized token supply");
   balances.set(initialOwner, TOTAL_SUPPLY);
-  storage.setItem("init", "done");
+  storage.set<string>("init", "done");
 }
 
 export function totalSupply(): string {
@@ -19,36 +19,47 @@ export function totalSupply(): string {
 }
 
 export function balanceOf(tokenOwner: string): u64 {
-  near.log("balanceOf: " + tokenOwner);
-  let result = balances.get(tokenOwner);
+  logging.log("balanceOf: " + tokenOwner);
+  if (!balances.contains(tokenOwner)) {
+    return 0;
+  }
+  let result = balances.getSome(tokenOwner);
   return result;
 }
 
 export function allowance(tokenOwner: string, spender: string): u64 {
-  return approves.get(tokenOwner + ":" + spender);
+  const key = tokenOwner + ":" + spender;
+  if (!approves.contains(key)) {
+    return 0;
+  }
+  return approves.getSome(key);
 }
 
 export function transfer(to: string, tokens: u64): boolean {
-  near.log("transfer from: " + context.sender + " to: " + to + " tokens: " + tokens.toString());
-  let fromAmount = balances.get(context.sender);
+  logging.log("transfer from: " + context.sender + " to: " + to + " tokens: " + tokens.toString());
+  let fromAmount = getBalance(context.sender);
   assert(fromAmount >= tokens, "not enough tokens on account");
   balances.set(context.sender, fromAmount - tokens);
-  balances.set(to, balances.get(to) + tokens);
+  balances.set(to, getBalance(to) + tokens);
   return true;
 }
 
 export function approve(spender: string, tokens: u64): boolean {
-  near.log("approve: " + spender + " tokens: " + tokens.toString());
+  logging.log("approve: " + spender + " tokens: " + tokens.toString());
   approves.set(context.sender + ":" + spender, tokens);
   return true;
 }
 
 export function transferFrom(from: string, to: string, tokens: u64): boolean {
-  let fromAmount = balances.get(from);
+  let fromAmount = getBalance(from);
   assert(fromAmount >= tokens, "not enough tokens on account");
-  let approvedAmount = approves.get(from + ":" + to);
+  let approvedAmount = allowance(from, to);
   assert(tokens <= approvedAmount, "not enough tokens approved to transfer");
   balances.set(from, fromAmount - tokens);
-  balances.set(to, balances.get(to) + tokens);
+  balances.set(to, getBalance(to) + tokens);
   return true;
+}
+
+function getBalance(owner: string) : u64 {
+  return balances.contains(owner) ? balances.getSome(owner) : 0;
 }
